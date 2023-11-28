dc929fcc9938194599d9b4e7097a787a4e239852
Ran3022 <139870642+Ran3022@users.noreply.github.com>
2023-11-28 21:39:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // complete the given function
 
 function palindrome(str){
+	  s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
+    // Compare the original string with its reverse
+    return s === s.split('').reverse().join('');
 }
 module.exports = palindrome
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
